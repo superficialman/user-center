f42f7fac9115c44dbf6d14cda40ed33da6ece322
maka-baka <2062940170@qq.com>
2025-03-27 12:34:35 +0800

    null

--- user-center-boot/src/main/java/com/bytebreeze/usercenterboot/entity/Article.java
+++ user-center-boot/src/main/java/com/bytebreeze/usercenterboot/entity/Article.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
 
@@ -13,7 +16,10 @@ import lombok.Data;
  */
 @TableName(value ="article")
 @Data
-public class Article {
+public class Article implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
     /**
      * 文章ID
      */
